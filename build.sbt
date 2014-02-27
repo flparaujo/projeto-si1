8b88da4607f6c9d668a62cb36b464817454d87b5
Felipe Araujo de Andrade 21211043 <felipeaa@lcc2-32.lcc.ufcg.edu.br>
2014-02-27 12:02:41 -0300

    nobody needs

--- build.sbt
+++ build.sbt
@@ -6,6 +6,10 @@ libraryDependencies ++= Seq(
   javaJdbc,
   javaEbean,
   cache
-)     
+)  
+
+mappings in (Compile, packageBin) += {
+ (baseDirectory.value / "disciplinas-do-curso.xml") -> "disciplinas-do-curso.xml"
+}
 
 play.Project.playJavaSettings
--- target/scala-2.10/src_managed/main/routes_routing.scala
+++ target/scala-2.10/src_managed/main/routes_routing.scala
@@ -1,6 +1,6 @@
-// @SOURCE:C:/Users/Win 7/si1/meu-plano-de-curso/conf/routes
+// @SOURCE:/home/felipeaa/herokuProjeto/projeto-si1/conf/routes
 // @HASH:e7509f0b490347ac336a2ed26e80801c9d543e5e
-// @DATE:Wed Feb 26 20:42:24 GMT-03:00 2014
+// @DATE:Thu Feb 27 11:51:22 BRT 2014
 
 
 import play.core._
